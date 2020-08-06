ee5091badc9f775448fc90d92fa38f42470b1d31
Shadab <shadab19it@gmail.com>
2020-08-06 22:54:23 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import { fetchWeather } from "./service/fetchWeather";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
+import Loader from "react-loader-spinner";
 
-function App() {
+const OnlineChecker = ({ isOline }) => {
+  if (isOline === "onn") {
+    return (
+      <h2 className='online'>
+        <div className='onn'>You are back to Online</div>
+      </h2>
+    );
+  } else if (isOline === "off") {
+    return (
+      <h2 className='online'>
+        <div className='off'>You are offline</div>
+      </h2>
+    );
+  } else {
+    return <div></div>;
+  }
+};
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+  const [isOline, setOnline] = useState("");
+
+  window.addEventListener("online", () => {
+    setOnline("onn");
+  });
+  window.addEventListener("offline", () => {
+    setOnline("off");
+  });
+
+  const onError = (err) => {
+    setError(err);
+  };
+
+  setTimeout(() => {
+    setOnline("");
+  }, 6000);
+
+  const Search = async (e) => {
+    if (e.key === "Enter" || e === "Enter") {
+      setLoading(true);
+      if (query !== "") {
+        const data = await fetchWeather(query, onError);
+        console.log(data);
+        setWeather(data);
+        setQuery("");
+        setLoading(false);
+        if (data) {
+          setError("");
+        }
+      } else {
+        setLoading(false);
+        setError("Please Enter any City Name");
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <OnlineChecker isOline={isOline} />
+      <div className='weather-app'>
+        <h1 className='title'>Search Weather Report !</h1>
+        {error ? (
+          <div className='error-box'>
+            <h2 className='error'>{error}</h2>
+          </div>
+        ) : (
+          ""
+        )}
+        <div className='search-box'>
+          <div className='icon' onClick={() => Search("Enter")}>
+            <BsSearch />
+          </div>
+          <input
+            type='text'
+            placeholder='Search..'
+            value={query}
+            className='search-field'
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyPress={Search}
+          />
+        </div>
+        {loading ? (
+          <div className='loader'>
+            <Loader type='TailSpin' color='#2d6cdf' height={80} width={80} />
+          </div>
+        ) : (
+          ""
+        )}
+
+        {weather && (
+          <div className='details-box'>
+            <div className='name'>
+              <span>{weather.name}</span>
+              <sup>{weather.sys.country}</sup>
+            </div>
+            <div className='city-temp'>
+              {Math.round(weather.main.temp)}
+              <sup>&deg;C</sup>
+            </div>
+            <div className='icon'>
+              <img src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} alt='weather-icon' />
+            </div>
+
+            <div className='desc'>{weather?.weather[0].description}</div>
+          </div>
+        )}
+      </div>
+      <div className='footer'>Shadab &copy; 2020</div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/service/fetchWeather.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_key = "c3b214593b0229751cae1e5773975c42";
+
+export const fetchWeather = async (query, onError) => {
+  try {
+    console.log(query);
+    const { data } = await axios(`${URL}${query}&appid=${API_key}`);
+    return data;
+  } catch (err) {
+    onError("Pleace Enter a Valid  City Name");
+  }
+};
